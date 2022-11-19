2b8c4a44f3c11d63a605e05dd6d076b883dff222
Truong <khungking909@gmail.com>
2022-11-19 10:22:18 +0700

    GUI FIX

---
+++ app/src/main/java/com/midterm/appnauan1/MainActivity.java
@@ -0,0 +1,30 @@
+package com.midterm.appnauan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.midterm.appnauan1.databinding.ActivityMainBinding;
+import com.midterm.appnauan1.databinding.ActivityMainFoodBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ConstraintLayout btnWelcome;
+    private ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        btnWelcome = findViewById(R.id.btn_welcome);
+        btnWelcome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MainFood.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
